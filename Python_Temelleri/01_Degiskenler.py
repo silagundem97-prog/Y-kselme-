4eb02a8c67282d49d7b1e9daf993b1c4cde56844
silagundem97-prog <silagundem97@gmail.com>
2025-11-10 13:55:01 +0300

    Create 01_Degiskenler.py

---
+++ Python_Temelleri/01_Degiskenler.py
@@ -0,0 +1,12 @@
+# Yükseliş Projesi - Python Temelleri: 1. Ders (Değişkenler)
+
+# 1. string (metin) değişkeni tanımlama
+isim = "Silagundem"
+
+# 2. integer (tam sayı) değişkeni tanımlama
+yas = 18 
+
+# 3. Sonucu ekrana yazdırma
+print("Merhaba, benim adım " + isim)
+print("Yaşım: " + str(yas))
+$$\text{feat: Python Temelleri klasörü ve değişkenler dersi eklendi}$$
